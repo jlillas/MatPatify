fea1111fe946c29a72d1a48529eb48435fabaef3
jlillas <60442424+jlillas@users.noreply.github.com>
2024-03-10 13:42:34 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Youtube MatPatify
+
+Use MatPatify to make your YouTube browsing more enjoyable. Is using the YouTube platform boring? Do you struggle to discover entertaining click-bait videos? avert your fears. Every thumbnail gets a MatPat styled thumbnail thanks to this plugin.
+
+Instructions on how to port and modify the extension can be found [here](https://github.com/MagicJinn/MrBeastify-Youtube/issues/16).
+
+Compatible with all browsers (Firefox and Chromium based).
+
+Notes:
+* This extension was inspired by [Unnecessary Inventions](http://www.youtube.com/@UnnecessaryInventions) and his [website](http://www.mrbeastify.com/).
+* This extension is unofficial and not affiliated with MatPat or YouTube.
+* This extension was made by @surely_somebody and you can find her at (https://twitter.com/surely_somebody) and (https://www.youtube.com/@surelysylvie).
+* I have done no work on this extension outside adding to the README, Manifest, and publishing it on extension sites. 
+* Sites published on can be found under. 
+* Chrome: (https://chrome.google.com/webstore/detail/matpatify/cgicnmnonpkpbgmboekkjdbckihlhajf?gl=IN).
+* Firefox: (https://addons.mozilla.org/en-US/firefox/addon/matpatify/).
---
+++ theory.js
@@ -0,0 +1,107 @@
+const imagesPath = "images/";
+
+// Apply the overlay
+function applyOverlay(thumbnailElement, overlayImageUrl, flip) {
+  // Create a new img element for the overlay
+  const overlayImage = document.createElement("img");
+  overlayImage.src = overlayImageUrl;
+  overlayImage.style.position = "absolute";
+  overlayImage.style.top = "0";
+  overlayImage.style.left = "0";
+  overlayImage.style.width = "100%";
+  overlayImage.style.height = "100%";
+  overlayImage.style.zIndex = "0"; // Ensure overlay is on top but below the time indicator
+  thumbnailElement.style.position = "relative"; // Style the thumbnailElement to handle absolute positioning
+
+  // Append the overlay to the parent of the thumbnail
+  thumbnailElement.parentElement.appendChild(overlayImage);
+}
+
+// Looks for all thumbnails and applies overlay
+function applyOverlayToThumbnails() {
+  // Query all YouTube video thumbnails on the page that haven't been processed yet
+  // (ignores shorts thumbnails)
+  const elementQuery =
+    "ytd-thumbnail:not(.ytd-video-preview, .ytd-rich-grid-slim-media) a > yt-image > img.yt-core-image:only-child:not(.yt-core-attributed-string__image-element)";
+  const thumbnailElements = document.querySelectorAll(elementQuery);
+
+  // Apply overlay to each thumbnail
+  thumbnailElements.forEach((thumbnailElement) => {
+    // Apply overlay and add to processed thumbnails
+    let loops = Math.random() > 0.001 ? 1 : 20; // Easter egg
+
+    for (let i = 0; i < loops; i++) {
+      // Get overlay image URL from your directory
+      const overlayImageUrl = getRandomImageFromDirectory();
+      const flip = Math.random() < 0.25; // 25% chance to flip the image
+      applyOverlay(thumbnailElement, overlayImageUrl, flip);
+    }
+  });
+}
+
+// Get the URL of an image
+function getImageURL(index) {
+  return chrome.runtime.getURL(`${imagesPath}${index}.png`);
+}
+
+// Get a random image URL from a directory
+function getRandomImageFromDirectory() {
+  const randomIndex = Math.floor(Math.random() * highestImageIndex) + 1;
+
+  return getImageURL(randomIndex);
+}
+
+// Checks if an image exists in the image folder
+async function checkImageExistence(index = 1) {
+  const testedURL = getImageURL(index)
+
+  try {
+    // See if the image exists
+    await fetch(testedURL);
+    return true // Image exists
+  } catch {
+    return false // Image does not exist
+  }
+}
+
+let highestImageIndex;
+// Gets the highest index of an image in the image folder starting from 1
+async function getHighestImageIndex() {
+  // Avoid exponential search for smaller values
+  let i = 4;
+
+  // Increase i until i is greater than the number of images
+  while (await checkImageExistence(i)) {
+    i *= 2;
+  }
+
+  // Possible min and max values
+  let min = i <= 4 ? 1 : i / 2;
+  let max = i;
+
+  // Binary search
+  while (min <= max) {
+    // Find the midpoint of possible max and min
+    let mid = Math.floor((min + max) / 2);
+
+    // Check if the midpoint exists
+    if (await checkImageExistence(mid)) {
+      // If it does, next min to check is one greater
+      min = mid + 1;
+    } else {
+      // If it doesn't, max must be at least one less
+      max = mid - 1;
+    }
+  }
+
+  // Max is the size of the image array
+  highestImageIndex = max;
+}
+
+getHighestImageIndex()
+  .then(() => {
+    setInterval(applyOverlayToThumbnails, 100);
+    console.log(
+      "MrBeastify Loaded Successfully, " + highestImageIndex + " images detected."
+    );
+  })
